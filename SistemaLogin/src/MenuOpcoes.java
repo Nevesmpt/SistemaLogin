bd1042fa19997116aaa50647850340445c3c77b8
Manuel Neves <manuelmneves@hotmail.com>
2022-10-31 15:45:55 +0000

    Preenche Tabela Final

--- SistemaLogin/src/MenuOpcoes.java
+++ SistemaLogin/src/MenuOpcoes.java
@@ -125,9 +125,9 @@ public class MenuOpcoes extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        jTable1 tabela = new jTable1();
+        MostraUtilizadores tabela = new MostraUtilizadores();
         this.setVisible(false);
-        tabela.setVisible(true);   
+        tabela.setVisible(true);  
         
      
         
@@ -163,6 +163,10 @@ public class MenuOpcoes extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+         
+        
+        
+        
         
         ArrayList<String> dados = new ArrayList<String>(); 
 
